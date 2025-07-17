7a765ece9fde7e411eb34009ee5f0d2214716dce
imeldaputr <imel.kein.email@gmail.com>
2025-07-17 11:00:09 +0200

    inserted more datas, edited the Book model, added one migrations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -7,4 +8,10 @@ from django.db import models
 class Book(models.Model):
     # Define the structure and its types of data (https://docs.djangoproject.com/en/5.2/ref/models/fields/)
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])  # Rating between 1 and 5
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+    
+    def __str__(self): # How the instances be output in the terminal
+        return f"{self.title} ({self.rating})"
